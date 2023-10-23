8914f259be5939da32f1b029b14ba980aab0e1b2
vebbis321 <veb.dan@hotmail.com>
2023-10-23 12:35:53 +0200

    added p10k and updated install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,20 @@
-sudo dnf install git vim neovim zsh stow polybar rofi alacritty nitrogen
+sudo dnf install neovim zsh polybar rofi alacritty nitrogen fzf ripgrep clang flatpak -y
+
+# zap
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
+
+# nvm 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# install node
+nvm install --lts
+
+# swap caps
+localectl set-x11-keymap us '' '' caps:escape
+
+# rpm
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf groupupdate core
+
+# flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
